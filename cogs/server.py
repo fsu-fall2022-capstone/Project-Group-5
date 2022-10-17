5f259b63a6e05944694ea311e2f58dbf1f779f74
Nicholas Green <friolasoda@gmail.com>
2022-10-17 14:47:31 -0400

    added command to fetch all public shards
    can't see command in discord, so unable to test (not sure if sync error/.. ?)

--- cogs/server.py
+++ cogs/server.py
@@ -1,8 +1,11 @@
-from typing import Literal, Optional
+from dis import disco
+from typing import Literal, Optional, List
+import aiohttp
 
 import discord
 from discord import app_commands
 from discord.ext import commands
+from requests import session
 
 from DAOs.nationstates_api import NationStatesAPI
 from nationstates_bot import NationStatesBot
@@ -16,7 +19,22 @@ class Server(commands.Cog):
         self.bot = bot
 
         self.logger = Logger("server")
-        self.logger.info("server loaded")
+        self.logger.info("server loaded")  
+
+    @app_commands.command(name="shards")
+    @app_commands.describe(
+        nation ="Name of the nation to be targeted",
+        shards ="List of shards to find"
+        )
+    async def fetch_shards(
+        self,
+        interaction: discord.Interaction,
+        nation: str,
+        shards: str,
+        ):
+        async with NationStatesAPI as session:
+            async with session.get(f'https://www.nationstates.net/cgi-bin/api.cgi?nation={nation}') as resp:
+                await interaction.response.send_message(await resp.text() )
 
 
 async def setup(bot: NationStatesBot):
