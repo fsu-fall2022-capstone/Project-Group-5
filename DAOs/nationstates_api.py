99bd76a3851bf22f356460cd5fba3a6e8f954efe
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-10-17 19:16:31 -0400

    public shards setup

--- DAOs/nationstates_api.py
+++ DAOs/nationstates_api.py
@@ -2,5 +2,24 @@ from aiohttp import ClientSession
 
 
 class NationStatesAPI:
+
+    BASE_URL = "https://www.nationstates.net/cgi-bin/api.cgi"
+
     def __init__(self, web_client: ClientSession) -> None:
         self.web_client = web_client
+
+    async def get_public_nation_data(self, nation: str, shards: list[str]):
+        params = {"nation": nation, "q": "+".join(shards)}
+        async with self.web_client.get(
+            self.BASE_URL,
+            headers={'User-Agent': 'NS Discord Bot'},
+            params=params,
+        ) as response:
+            if response.ok:
+                return await response.text()
+
+
+# url = "https://www.nationstates.net/cgi-bin/api.cgi"
+# p = {"nation" : "Computer Chip", "q":"policies"}
+# h = {'User-Agent': 'Testing for now'}
+# a = requests.get(url, headers=h, params=p)
--- controllers/server_controller.py
+++ controllers/server_controller.py
@@ -1,4 +1,3 @@
-import os
 import traceback
 from typing import Literal
 
@@ -6,8 +5,10 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-from controllers.base_controller import BaseController
+from controllers.base_nationstate_controller import BaseNationstateController
 
 
-class ServerController(BaseController):
-    pass
+class ServerController(BaseNationstateController):
+    async def info(self, interaction: discord.Interaction, nation: str, shard: str):
+        data = await self.bot.nationstates_api.get_public_nation_data(nation, shards=[shard])
+        await interaction.response.send_message(embed=discord.Embed(title=nation, description=data))
