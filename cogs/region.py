ff2558c39fdfca5adf893f13dbad681c46c8f399
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-10-18 22:23:26 -0400

    clean up region cog

--- cogs/region.py
+++ cogs/region.py
@@ -1,14 +1,14 @@
 from typing import Literal, Optional
 
 import discord
+from controllers.region_controller import RegionController
 from discord import app_commands
 from discord.ext import commands
-
-from controllers.region_controller import RegionController
 from nationstates_bot import NationStatesBot
 from utils.logger import Logger
 
-class Region(commands.GroupCog, group_name="Region"):
+
+class Region(commands.GroupCog, group_name="region"):
     """Handle region requests"""
 
     def __init__(self, bot: NationStatesBot):
@@ -29,7 +29,7 @@ class Region(commands.GroupCog, group_name="Region"):
     async def info(
         self, interaction: discord.Interaction, region: str, shard: Optional[str] = None
     ):
-        self.logger.info(f"starting to fetch data with {region=} and {shard=}")
+        self.logger.info(f"starting to fetch data with {region = } and {shard = }")
         await self.region_controller.info(interaction, region, shard)
         self.logger.info(f"fetched {shard} for {region = }")
 
--- controllers/region_controller.py
+++ controllers/region_controller.py
@@ -2,24 +2,24 @@ import traceback
 from typing import Literal, Optional
 
 import discord
+from data.shards import VALID_REGION_SHARDS
 from discord import app_commands
 from discord.ext import commands
 
 from controllers.base_nationstate_controller import BaseNationstateController
-from controllers.shards import VALID_REGION_SHARDS
+
 
 class RegionController(BaseNationstateController):
     @classmethod
     async def region_shards_autocomplete(
-        cls, interaction: discord.Interaction, current: str
+        cls, _: discord.Interaction, current: str
     ) -> list[app_commands.Choice[str]]:
-        return cls.shard_autocomplete[current,VALID_REGION_SHARDS]
+        return cls.shard_autocomplete(current, VALID_REGION_SHARDS)
 
-    @classmethod
     async def info(
         self, interaction: discord.Interaction, region: str, shard: Optional[str] = None
     ):
         data = await self.bot.nationstates_api.get_region_data(
-            region, shards = [shard] if shard else None
+            region, shards=[shard] if shard else None
         )
-        await interaction.response.send_messag(embed=discord.Embed(title=region,description=data))
+        await interaction.response.send_message(embed=discord.Embed(title=region, description=data))
