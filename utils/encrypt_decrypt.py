9bba1dc5eddd9534600cb46f3dec5146ef6e366e
tpm18b <tpm18b@fsu.edu>
2022-10-21 10:01:14 -0400

    cleaned up encrypt and decrypt functions

--- utils/encrypt_decrypt.py
+++ utils/encrypt_decrypt.py
@@ -4,13 +4,15 @@ import os
 
 
 def decrypt(encrypted_password):
-    fernet = Fernet(os.getenv("s_key"))  #pass the secret key to fernet to create an instance
-    decryptPassword = fernet.decrypt(
+    fernet = Fernet(os.getenv("s_key"))    # pass the secret key to fernet to create an instance
+    decryptedPassword = fernet.decrypt(    # decrypts encrypted_password and sets it equal to decryptedPassword
         encrypted_password              
     ).decode()
+    return decryptedPassword                
 
 def encrypt(password):
-    fernet = Fernet(os.getenv("s_key"))
-    encryptedPassword = fernet.encrypt(
+    fernet = Fernet(os.getenv("s_key"))     # pass the secret key to fernet to create an instance
+    encryptedPassword = fernet.encrypt(     # ecnrypts the password and sets it equal to encryptedPassword
         password.encode()
     )
+    return encryptedPassword
