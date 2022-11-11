8eb2617c99c5fd869b3890fef87f027be67b75e0
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-11-11 04:28:43 -0500

    db init for tracking nations

--- ns_bot/postgres_init.py
+++ ns_bot/postgres_init.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 
 import asyncpg
+from asyncpg import Connection
 from dotenv import load_dotenv
 
 
@@ -9,6 +10,7 @@ async def db_init():
     async with asyncpg.create_pool(
         database="nations", user=os.environ.get("USER"), command_timeout=60
     ) as pool:
+        con: Connection
         async with pool.acquire() as con:
             # Command Usage
             await con.execute(
@@ -16,6 +18,28 @@ async def db_init():
                 CREATE TABLE IF NOT EXISTS command_usage (
                     command text PRIMARY KEY,
                     usage_count integer DEFAULT 0
+                )
+                """
+            )
+
+            await con.execute(
+                """
+                CREATE TABLE IF NOT EXISTS login (
+                    nation text PRIMARY KEY not null,
+                    password text not null,
+                    pin text
+                )
+                """
+            )
+
+            await con.execute(
+                """
+                CREATE TABLE IF NOT EXISTS nation (
+                    nation text PRIMARY KEY not null,
+                    id bigint not null,
+                    dm bool not null,
+                    vote_time int default -1,
+                    vote_channel bigint
                 )
                 """
             )
