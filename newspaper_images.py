1aeb9ea926ed24d0c474b1d6d3b70faded0467ea
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-11-20 12:53:52 -0500

    Convert into function

--- newspaper_images.py
+++ newspaper_images.py
@@ -1,88 +1,83 @@
 import asyncio
-import os
-import sys
+from io import BytesIO
 
-import requests
 from aiohttp import ClientSession
 from PIL import Image, ImageDraw, ImageFont
 
-nation_name = "NATION"
-nation_currency = "CURRENCY"
-article_title = "Title Here!"
-
-image_url = "https://www.nationstates.net/images/"
+BASE_IMAGE_URL = "https://www.nationstates.net/images/"
 
 
-async def get_images():
+async def generate_issue_newspaper(
+    web_session: ClientSession,
+    nation: str,
+    currency: str,
+    article_title: str,
+    banner_1: str,
+    banner_2: str,
+    flag,
+):
     results = []
-    urls = [
-        "flags/uploads/computer_chip__602300t1.png",
-        "newspaper/t1-1.jpg",
-        "newspaper/s1-2.jpg",
-    ]
+    urls = [flag, f"newspaper/{banner_1}-1.jpg", f"newspaper/{banner_2}-2.jpg"]
+    for url in urls:
+        async with web_session.get(
+            BASE_IMAGE_URL + url, headers={"User-Agent": "NS Discord Bot"}
+        ) as response:
+            results.append(Image.open(BytesIO(await response.content.read())))
+
+    # TODO put these images in place
+    flag_image = results[0]
+    banner_1_image = results[1]
+    banner_2_image = results[2]
+
+    top_paper = Image.open("ns_bot/data/newspaper-references/paper1.png")
+    header_paper = Image.open("ns_bot/data/newspaper-references/paper2.png")
+    header_font = ImageFont.truetype("ns_bot/data/newspaper-references/UnifrakturCook-Bold.ttf", 25)
+    paper_name = ImageDraw.Draw(header_paper)
+    paper_name.text(
+        (header_paper.width / 3, 10),
+        f"The {nation} Chronicle",
+        font=header_font,
+        fill=(68, 68, 68),
+    )
+    currency_font = ImageFont.truetype("ns_bot/data/newspaper-references/times new roman.ttf", 10)
+    paper_name.text(
+        (header_paper.width - 120, 10),
+        f"1 {currency}",
+        font=currency_font,
+        fill=(68, 68, 68),
+    )
+
+    title_paper = Image.open("ns_bot/data/newspaper-references/paper4.png")
+    title_font = ImageFont.truetype("ns_bot/data/newspaper-references/times new roman.ttf", 30)
+    headline = ImageDraw.Draw(title_paper)
+    headline.text((35, 10), f"{article_title}", font=title_font, fill=(68, 68, 68))
+
+    bottom_paper = Image.open("ns_bot/data/newspaper-references/paper5.png")
+
+    total_height = top_paper.height + header_paper.height + title_paper.height + bottom_paper.height
+
+    paper_template = Image.new("RGBA", (bottom_paper.width, total_height))
+    paper_template.paste(top_paper, (0, 0))
+    paper_template.paste(header_paper, (0, top_paper.height))
+    paper_template.paste(title_paper, (0, top_paper.height + header_paper.height))
+    paper_template.paste(bottom_paper, (0, total_height - bottom_paper.height))
+    paper_template.paste(banner_1_image, (35, total_height - bottom_paper.height))
+
+    paper_template.show()
+
+
+async def main():
+    """TEMP Testing function"""
     async with ClientSession() as session:
-        for url in urls:
-            async with session.get(image_url + url) as response:
-                results.append(await response.text())
-        flag_image = results[0]
-        big_paper_image = results[1]
-        small_paper_image = results[2]
-    return results
+        await generate_issue_newspaper(
+            session,
+            "Group5",
+            "Dollars",
+            "Cloning Research Promises New Breakthrough",
+            "t1",
+            "s1",
+            "flags/uploads/computer_chip__602300t1.png",
+        )
 
 
-results = asyncio.run(get_images())
-
-top_paper = Image.open("ns_bot/data/newspaper-references/dpaper1.png")
-
-header_paper = Image.open("ns_bot/data/newspaper-references/dpaper2.png")
-header_font = ImageFont.truetype(
-    "ns_bot/data/newspaper-references/UnifrakturCook-Bold.ttf", 25
-)
-paper_name = ImageDraw.Draw(header_paper)
-paper_name.text(
-    (header_paper.width / 3, 10),
-    f"The {nation_name} Chronicle",
-    font=header_font,
-    fill=(68, 68, 68),
-)
-currency_font = ImageFont.truetype(
-    "ns_bot/data/newspaper-references/times new roman.ttf", 10
-)
-paper_name.text(
-    (header_paper.width - 120, 10),
-    f"1 {nation_currency}",
-    font=currency_font,
-    fill=(68, 68, 68),
-)
-
-title_paper = Image.open("ns_bot/data/newspaper-references/dpaper4.png")
-title_font = ImageFont.truetype(
-    "ns_bot/data/newspaper-references/times new roman.ttf", 50
-)
-headline = ImageDraw.Draw(title_paper)
-headline.text((35, 10), f"{article_title}", font=title_font, fill=(68, 68, 68))
-
-bottom_paper = Image.open("ns_bot/data/newspaper-references/dpaper5.png")
-
-total_height = (
-    top_paper.height + header_paper.height + title_paper.height + bottom_paper.height
-)
-
-paper_template = Image.new("RGBA", (bottom_paper.width, total_height))
-paper_template.paste(top_paper, (0, 0))
-paper_template.paste(header_paper, (0, top_paper.height))
-paper_template.paste(title_paper, (0, top_paper.height + header_paper.height))
-paper_template.paste(bottom_paper, (0, total_height - bottom_paper.height))
-
-paper_template.show()
-# nation_name
-# nation_city
-# nation_flag
-# nation_currency
-# download static images for newspaper
-# write headline of newspaper
-# flag, first, and second banner are variable, call from website itself
-
-# "draw" newspaper together using amalgamation
-# making a file but dont want to save: import io
-# font-family: unifrakturcook,times new roman,serif;
+results = asyncio.run(main())
