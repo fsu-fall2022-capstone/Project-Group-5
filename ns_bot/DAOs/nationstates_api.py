a9e4bc2a1fca33a8642b162b00d8b717203c60a9
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-11-22 14:38:47 -0500

    get list of issues from api

--- ns_bot/DAOs/nationstates_api.py
+++ ns_bot/DAOs/nationstates_api.py
@@ -72,6 +72,18 @@ class NationStatesAPI:
 
             return response.ok
 
+    async def get_nation_issues(self, nation: str):
+        password, pin = await self.login_table.get_nation_login(nation=nation)
+        async with self.web_client.get(
+            self.BASE_URL,
+            headers={"User-Agent": self.USER_AGENT, "X-password": password, "X-Pin": pin},
+            params={"nation": nation, "q": "issues"},
+        ) as response:
+            if pin := response.headers.get("X-Pin"):
+                await self.login_table.update_nation_pin(nation=nation, pin=pin)
+
+            return response.text()
+
     async def _rate_limit(self):
         # await asyncio.sleep(0.1)
         return
