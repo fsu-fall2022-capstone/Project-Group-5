9e0125a18292d758d77b4394208d00cc579a5258
Nicholas Green <friolasoda@gmail.com>
2022-11-24 14:44:28 -0500

    fixed limited fields
    will display all information from the nation/region in embeds with 10 fields each

--- ns_bot/utils/formatter.py
+++ ns_bot/utils/formatter.py
@@ -1,15 +1,32 @@
+
 import discord
 import xmltodict
 
+from itertools import islice
+
 def format_xml(data):
-    dictionary = xmltodict.parse(data)
-    return build_embed(discord.Embed(), dictionary)
+    dictionary = flatten(xmltodict.parse(data))
+    dictionaries = split_dict(dictionary)
+    return build_embeds(dictionaries)
 
-def build_embed(embed, dictionary):
-    for key, value in dictionary.items():
-        if isinstance(value,dict):
-            build_embed(embed, value)
-        else:    
-            embed.add_field(name = str(key), value = str(value)) 
-    return embed     
+def flatten(nested_dict, sep =' ', pre =''):
+    return { pre + sep + key if pre else key : v
+             for kkey, vvalue in nested_dict.items()
+             for key, v in flatten(vvalue, sep, kkey).items()
+             } if isinstance(nested_dict, dict) else { pre : nested_dict }
 
+def split_dict(dictionary, size = 10):
+    it = iter(dictionary)
+    items = []
+    for i in range(0, len(dictionary), size):
+        items.append({key:dictionary[key] for key in islice(it,size)})
+    return items
+    
+def build_embeds(dictionaries):
+    embeds = []
+    for i in range(len(dictionaries)):
+        embed = discord.Embed()
+        for key, value in dictionaries[i].items():
+            embed.add_field(name = str(key), value = str(value))
+        embeds.append(embed) 
+    return embeds  
