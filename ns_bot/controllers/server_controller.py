26f96d4294e05c61668ab4f8afaf6b13b5cb0e07
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-11-25 14:58:15 -0500

    getting issues and responding to them

--- ns_bot/controllers/server_controller.py
+++ ns_bot/controllers/server_controller.py
@@ -1,11 +1,14 @@
 import datetime
+from io import BytesIO, StringIO
 
 import discord
+import xmltodict
 
 from nationstates_bot import NationStatesBot
 from ns_bot.controllers.base_nationstate_controller import BaseNationstateController
 from ns_bot.DAOs.postgresql import IssueVotes, LiveIssues, Login, Nation
 from ns_bot.formatting.newspaper_images import generate_issue_newspaper
+from ns_bot.utils import Logger, async_wrapper
 from ns_bot.views.configure import ConfigureNation, NewNation
 from ns_bot.views.issues import IssueView
 
@@ -24,6 +27,9 @@ class ServerController(BaseNationstateController):
         self.nation_table = nation_table
         self.live_issues_table = live_issues_table
         self.issue_votes_table = issue_votes_table
+        self.nation_dump = {}
+
+        self.logger = Logger("server_controller")
 
     async def add_nation(self, interaction: discord.Interaction):
         new_nation_view = NewNation(self.bot.nationstates_api, self.login_table, self.nation_table)
@@ -68,28 +74,29 @@ class ServerController(BaseNationstateController):
         article_title: str,
         banner_1: str,
         banner_2: str,
-        flag: str,
-        issue_number: str,
+        flag_url: str,
+        issue_number: int,
+        issue_summary: str,
+        options: list[str],
     ):
-        # TODO Remove temp things :
-        """
-        issue_id from xml
-        option_amount = len(xml.option)
-        """
         issue_channel = self.bot.get_channel(
             await self.nation_table.get_vote_channel(nation=nation)
         )
+        newspaper_image = await generate_issue_newspaper(
+            self.bot.web_client,
+            nation,
+            currency,
+            article_title,
+            banner_1,
+            banner_2,
+            flag_url,
+            issue_number,
+        )
+        newspaper_file = BytesIO()
+        newspaper_image.save(newspaper_file, format="PNG")
+        newspaper_file.seek(0)
         file = discord.File(
-            await generate_issue_newspaper(
-                self.bot.web_client,
-                nation,
-                currency,
-                article_title,
-                banner_1,
-                banner_2,
-                flag,
-                issue_number,
-            ),
+            newspaper_file,
             filename="image.png",
         )
         issue_embed = discord.Embed(title=article_title)
@@ -100,28 +107,54 @@ class ServerController(BaseNationstateController):
             name=f"{nation} Issue #{issue_number}", message=message
         )
 
-        issue_amount = 3
         embeds = [
             discord.Embed(
                 title=f"Option {i + 1}",
-                description="""TODO get the next issue we are going to send and fill out the options here""",
+                description=option,
             )
-            for i in range(issue_amount)
+            for i, option in enumerate(options)
         ]
+        embeds.insert(0, discord.Embed(title="The issue", description=issue_summary))
 
-        await thread.send(embeds=embeds, view=IssueView(issue_amount))
+        await thread.send(embeds=embeds, view=IssueView(len(options)))
         await self.live_issues_table.insert_issue(
             nation=nation, issue_id=issue_number, issue_channel=thread.id
         )
+        self.logger.info(f"sent {nation} {issue_number = } to {thread.id}")
 
     async def fetch_new_issues(self):
+        async_xmltodict = async_wrapper(xmltodict.parse)
+
         for nation in await self.nation_table.get_all():
-            issues = await self.bot.nationstates_api.get_nation_issues(nation["nation"])
-            nation_issues = await self.live_issues_table.get_nation_issues(nation=nation["nation"])
-            # if issue_id not in nation_issues:
-            #       generate_issue()
-            # loop through the issues and check if a new one is present
-            # or make another db (or edit current) to store the time till nation has new issue
+            nation_name: str = nation["nation"]
+            issues = await self.bot.nationstates_api.get_nation_issues(nation_name)
+            nation_issues = await self.live_issues_table.get_nation_issues(nation=nation_name)
+            issues = (await async_xmltodict(issues))["NATION"]["ISSUES"]["ISSUE"]
+            for issue in issues:
+                issue_id = int(issue["@id"])
+                if issue_id in nation_issues:
+                    continue
+
+                options = [option["#text"] for option in issue["OPTION"]]
+                nation_dump = self.nation_dump.get(nation_name, {})
+                flag = nation_dump.get(
+                    "FLAG", "https://www.nationstates.net/images/flags/default.jpg"
+                )
+                currency = nation_dump.get("CURRENCY", "Dollar")
+                flag = "/".join(flag.split("/")[4:])
+                await self.generate_issue(
+                    nation=nation_name,
+                    currency=currency,
+                    article_title=issue["TITLE"],
+                    banner_1=issue["PIC1"],
+                    banner_2=issue["PIC2"],
+                    flag_url=flag,
+                    issue_number=issue_id,
+                    issue_summary=issue["TEXT"],
+                    options=options,
+                )
+
+            # TODO remove issues that are already answered
 
     async def update_live_issues(self):
         # TODO optimize the search
@@ -152,13 +185,18 @@ class ServerController(BaseNationstateController):
 
         if respond_to_api:
             # TODO format the response from the API
-            issue_response_result += await self.bot.nationstates_api.respond_to_issue(
+            issue_response_result = await self.bot.nationstates_api.respond_to_issue(
                 nation, issue_id, option
             )
+            await channel.send(
+                file=discord.File(StringIO(issue_response_result), filename="response.xml")
+            )
+        else:
+            await channel.send(issue_response_result)
 
-        await channel.send(issue_response_result)
         await self.live_issues_table.remove_issue(nation=nation, issue_id=issue_id)
         await self.issue_votes_table.remove_issue(issue_channel=channel)
+        self.logger.info(f"{issue_id = } submitted {option = } for {nation = }")
 
     # async def data_dump(self):
     #     # TODO implement a fetch of the nation data dump for things like
