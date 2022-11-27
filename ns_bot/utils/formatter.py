be839fdb3aa8702ab762bdd8b780983da80d5c18
Nicholas Green <friolasoda@gmail.com>
2022-11-27 16:39:29 -0500

    added most shards
    most shards are now covered

--- ns_bot/utils/formatter.py
+++ ns_bot/utils/formatter.py
@@ -1,164 +1,188 @@
 
 import discord
-import random
 import xml.etree.ElementTree as ET
 
-from ns_bot.data.shards import VALID_PUBLIC_NATION_SHARDS
 from ns_bot.utils.wrappers import async_wrapper
 
-async def format_nation_info(data, shard):
+async def format_nation_info(data:str, shard):
     
     async_parse = async_wrapper(ET.fromstring)
-    root = await async_parse(data)
+    root = await async_parse(data)  
+    nation = ' '.join(root.attrib['id'].title().split('_'))
+    text = root[0].text
+    color = discord.Color.random()
+    national_currency = "UNKNOWN currency"
     if shard:
         match shard:
             case 'admirable':
-                print(len(VALID_PUBLIC_NATION_SHARDS))
-                return [discord.Embed(title=f"{root.attrib['id'].capitalize()}'s top trait is being {root[0].text.capitalize()}", color=discord.Color.dark_red())]
+                return [discord.Embed(title=f"The people of {nation} are known for being very {text}.", color=color)]
             case 'admirables':
-                embed=discord.Embed(title=f"Admirable Traits of {root.attrib['id'].capitalize()}", color=discord.Color.green())
+                embed=discord.Embed(title=f"Admirable Traits of {nation}", color=color)
                 for i in range(0, len(root[0])):
                     embed.add_field(name=f"Trait {i+1}",value=str(root[0][i].text.title()), inline=False)
                 return [embed]
             case 'animal':
-                return [discord.Embed(title=f"{root.attrib['id'].capitalize()}'s national animal is the: {root[0].text.title()}", color=discord.Color.blurple())]
+                return [discord.Embed(title=f"{nation}'s national animal is the: {text.title()}", color=color)]
             case 'animaltrait':
-                return [discord.Embed(title=f"The national animal of {root.attrib['id'].capitalize()} has this trait:", description=f"{root[0].text.capitalize()}", color=discord.Color.blurple())]
+                return [discord.Embed(title=f"The national animal of {nation} has this trait:", description=f"{text.capitalize()}", color=color)]
             case 'answered':
-                return [discord.Embed(title=f"{root.attrib['id'].capitalize()} has voted on {root[0].text.capitalize()} issues", color=discord.Color.gold())]
+                return [discord.Embed(title=f"{nation} has voted on {text.capitalize()} issues", color=color)]
             case 'banner':
                 pass
             case 'banners':
                 pass
             case 'capital':
-                return [discord.Embed(title=f"{root[0].text.title()} is the capital city of {root.attrib['id'].capitalize()} ", color=discord.Color.teal())]
+                return [discord.Embed(title=f"{text.title()} is the capital city of {nation} ", color=color)]
             case 'category':
-                return [discord.Embed(title=f"{root.attrib['id'].title()} is a {root[0].text} !", color=discord.Color.red())]
+                return [discord.Embed(title=f"{nation} is a {text}!", color=color)]
             case 'census':
-                embed=discord.Embed(title=f"{root.attrib['id'].title()}'s Public Education standing", color=discord.Color.fuchsia())
+                embed=discord.Embed(title=f"{nation}'s Public Education standing", color=color)
                 embed.add_field(name="World Ranking", value=root[0][0][1].text, inline=False)
                 embed.add_field(name="Rank within the Region", value=root[0][0][2].text, inline=False)
-                embed.add_field(name="Raw Score", value=root[0][0][0].text, inline=False)
+                embed.add_field(name="Score on the ", value=root[0][0][0].text, inline=False)
                 return [embed]
             case 'crime':
-                return [discord.Embed(title=f"Criminal activity in {root.attrib['id'].title()}", description=f"{root[0].text.capitalize()}")]
+                return [discord.Embed(title=f"Criminal activity in {nation}", description=f"{text.capitalize()}", color=color)]
             case 'currency':
-                return [discord.Embed(title=f"The {root[0].text.capitalize()} is the national currency of {root.attrib['id'].title()}")]
+                return [discord.Embed(title=f"The {text.capitalize()} is the national currency of {nation}", color=color)]
             case 'customleader':
                 return [discord.Embed(title="ERROR", description=data)]
             case 'customcapital':
-                return [discord.Embed(title="ERROR", description=data)]
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'customreligion':
-                return [discord.Embed(title="ERROR", description=data)]
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'dbid':
-                return [discord.Embed(title=f"Database ID for {root.attrib['id'].title()}: {root[0].text}")]
+                return [discord.Embed(title=f"Database ID for {nation}: {text}", color=color)]
             case 'deaths':
-                embed=discord.Embed(title=f"People die for {root.attrib['id'].title()}: {root[0].text}")
-                pass
+                embed=discord.Embed(title=f"Causes of death in {nation}", color=color)
+                for cause in root[0].iterfind('CAUSE'):
+                    embed.add_field(name=f"{cause.text}% of people die from:", value=f"{cause.attrib['type']}", inline=False)
+                return [embed]
             case 'demonym':
-                pass
+                return [discord.Embed(title=f"A person from {nation} is a {text}", color=color)]
             case 'demonym2':
-                pass
+                return [discord.Embed(title=f"Another common name for a person from {nation} is {text}", color=color)]
             case 'demonym2plural':
-                pass
+                return [discord.Embed(title=f"A group of people from {nation} are called {text}", color=color)]
             case 'dispatches':
-                pass
+                return [discord.Embed(title=f"{nation} has had {text} dispatches", color=color)]
             case 'dispatchlist':
-                pass
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'endorsements':
-                pass
+                if text:
+                    return [discord.Embed(title=f"{nation} has been endorsed by {len(root[0].findall('ENDORSEMENT'))} nations", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation} has been endorsed by zero nations!", color=color)] 
             case 'factbooks':
-                pass
+                return [discord.Embed(title=f"{nation} has a total of {text} factbooks written about them.", color=color)]
             case 'factbooklist':
-                pass
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'firstlogin':
-                pass
+                return [discord.Embed(title=f"The nation of {nation} was first logged in on {text}.", color=color)]
             case 'flag':
-                pass
+                embed = discord.Embed(title=f"The flag of {nation}", url=text, color=color)
+                return [embed]
             case 'founded':
-                pass
+                return [discord.Embed(title=f"{nation} was founded exactly {text}!", color=color)]
             case 'foundedtime':
-                pass
+                return [discord.Embed(title=f"The nation of {nation} was founded at {text}.", color=color)]
             case 'freedom':
-                pass
+                sentence = f"The nation of {nation} has a {root[0][0].text.lower()} record of civil rights, an economy considered by many to be {root[0][1].text.lower()}, and a political system that is {root[0][2].text.lower()}."
+                return [discord.Embed(title=sentence, color=color)]
             case 'fullname':
-                pass
+                return [discord.Embed(title=f"The proper name of {nation} is '{text}'.", color=color)]
             case 'gavote':
-                pass
+                return [discord.Embed(title=f"{nation}'s status in the General Assembly is currently {text}.", color=color)]
             case 'gdp':
-                pass
+                return [discord.Embed(title=f"The nation of {nation} has a GDP of {text} {national_currency}!", color=color)]
             case 'govt':
                 pass
             case 'govtdesc':
-                pass
+                return [discord.Embed(title=text, color=color)]
             case 'govtpriority':
-                pass
+                return [discord.Embed(title=f"The government of {nation} prioritizes their {text} above all else.", color=color)]
             case 'happenings':
-                pass
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'income':
-                pass
+                return [discord.Embed(title=f"The average income for a resident of {nation} is {text} {national_currency}.", color=color)]
             case 'industrydesc':
-                pass
+                return [discord.Embed(title=text, color=color)]
             case 'influence':
-                pass
+                return [discord.Embed(title=f"The nation of {nation} spreads its influence by being a {text}.", color=color)]
             case 'lastactivity':
-                pass
+                return [discord.Embed(title=f"{nation} had their most recent activity {text}.", color=color)]
             case 'lastlogin':
-                pass
+                return [discord.Embed(title=f"{nation} was last logged in {text}.", color=color)]
             case 'leader':
-                pass
+                return [discord.Embed(title=f"{text} is the current leader of {nation}.", color=color)]
             case 'legislation':
-                pass
+                return [discord.Embed(title="ERROR", description=data, color=color)]
             case 'majorindustry':
-                pass
+                return [discord.Embed(title=f"{text} is the biggest industry of {nation}!", color=color)]
             case 'motto':
-                pass
+                return [discord.Embed(title=f'"{text}"' + f" is the motto of {nation}.", color=color)]
             case 'name':
-                pass
+                return [discord.Embed(title=nation, color=color)]
             case 'notable':
-                pass
+                return [discord.Embed(title=f"{nation} is known for their {text}.", color=color)]
             case 'notables':
                 pass
             case 'policies':
                 pass
             case 'poorest':
-                pass
+                return [discord.Embed(title=f"The poorest 10% of people in {nation} have a reported income of  {text} {national_currency}.", color=color)]
             case 'population':
-                pass
+                if len(text) < 4:
+                    return [discord.Embed(title=f"{nation} has a population of {text} million", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation} has a population of {float(text) / 1000} billion.", color=color)]
             case 'publicsector':
-                pass
+                [discord.Embed(title=f"{text}% of the economy is owned by the government of {nation}.", color=color)]
             case 'rcensus':
                 pass
             case 'region':
-                pass
+                return [discord.Embed(title=f"{nation} can be found in the {text} region.", color=color)]
             case 'religion':
-                pass
+               return [discord.Embed(title=f"{text} is the official religion of {nation}.", color=color)]
             case 'richest':
-                pass
+                return [discord.Embed(title=f"The richest 10% of people in {nation} have a reported income of  {text} {national_currency}.", color=color)]
             case 'scvote':
-                pass
+                if text:
+                    return [discord.Embed(title=f"{nation}'s status on the Security Council is currently {text}.", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation} is not a member of the Security Council.", color=color)]
             case 'sectors':
                 pass
             case 'sensibilities':
-                pass
+                return [discord.Embed(title=f"The people of {nation} are known to be {text}.", color=color)]
             case 'tax':
-                pass
+                return [discord.Embed(title=f"The average tax rate in {nation} is {text}%.", color=color)]
             case 'tgcanrecruit':
-                pass
+                if text == '1':
+                    return [discord.Embed(title=f"{nation} can receive recruitment telegrams.", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation} can not receive recruitment telegrams.", color=color)]
             case 'tgcancampaign':
-                pass
+                if text == '1':
+                    return [discord.Embed(title=f"{nation} can receive campaign telegrams.", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation} can not receive campaign telegrams.", color=color)]
             case 'type':
-                pass
+                return [discord.Embed(title=f"{nation} is a {text}!", color=color)]
             case 'wa':
-                pass
+                return [discord.Embed(title=f"{nation}'s membership status in the World Assmeby is: {text}.", color=color)]
             case 'wabadges':
-                pass
+                if text:
+                    return [discord.Embed(title=f"{nation}'s has the following badges from the world assembly {text}.", color=color)]
+                else:
+                    return [discord.Embed(title=f"{nation}'s has no from the world assembly.", color=color)]
             case 'wcensus':
                 pass
             case 'zombie':
                 pass
     else:
+        embeds = []
         return [discord.Embed(title="Nation Info", description=data)]
 
 def format_region_info(data,shard):
-    return discord.Embed(title="Region Info", description=shard)
+    return discord.Embed(title="Region Info", description=data)
