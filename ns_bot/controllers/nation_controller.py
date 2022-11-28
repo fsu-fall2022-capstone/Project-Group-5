6f67fec555223dd2af511f1cfb7f5320f831e832
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-11-27 20:50:21 -0500

    isort

--- ns_bot/controllers/nation_controller.py
+++ ns_bot/controllers/nation_controller.py
@@ -4,11 +4,11 @@ from typing import Literal, Optional
 import discord
 from discord import app_commands
 from discord.ext import commands
-from ns_bot.utils.formatter import format_nation_info
-from ns_bot.utils.wrappers import async_wrapper
+
 from ns_bot.controllers.base_nationstate_controller import BaseNationstateController
 from ns_bot.data.shards import VALID_PUBLIC_NATION_SHARDS
-
+from ns_bot.utils.formatter import format_nation_info
+from ns_bot.utils.wrappers import async_wrapper
 
 
 class NationController(BaseNationstateController):
@@ -24,5 +24,5 @@ class NationController(BaseNationstateController):
         data = await self.bot.nationstates_api.get_public_nation_data(
             nation, shards=[shard] if shard else None
         )
-        embeds = await format_nation_info(data,shard)
+        embeds = await format_nation_info(data, shard)
         await interaction.response.send_message(embeds=embeds)
