22e2b207fe671d9f35145d12996406137954a23a
tpm18b <tpm18b@fsu.edu>
2022-12-06 20:34:13 -0500

    added shard formatting

--- ns_bot/formatting/region_info.py
+++ ns_bot/formatting/region_info.py
@@ -9,7 +9,7 @@ from PIL import Image
 from ns_bot.formatting import Formatter
 from ns_bot.utils.wrappers import async_wrapper
 
-
+# test push
 class FormatRegionInfo(Formatter):
     @classmethod
     async def format(
@@ -29,6 +29,7 @@ class FormatRegionInfo(Formatter):
                 await interaction.response.send_message(
                     embed=discord.Embed(title=f"{region} has no dispatch list.")
                 )
+
         match shard:
             case "banner":
                 embed = discord.Embed(title=f"Banner of {region}.")
@@ -37,22 +38,37 @@ class FormatRegionInfo(Formatter):
                 await interaction.response.send_message(embed=embed)
             case "bannerby":
                 await interaction.response.send_message(
-                    embed=discord.Embed(title=f"{region}'s region banner was created by:  {text}")
+                    embed=discord.Embed(title=f"{region}'s region banner was created by: {text}")
                 )
             case "bannerurl":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title="Banner URL", description=text, url=cls.BASE_REGION_URL + text
+                    )
+                )
             case "census":
-                pass
+                embed = discord.Embed(title=f"{region}'s __ standing")
+                embed.add_field(name="World Ranking", value=root[0][0][1].text, inline=False)
+                embed.add_field(name="Score", value=root[0][0][0].text, inline=False)
+                await interaction.response.send_message(embed=embed)
             case "censusranks":
                 pass
             case "dbid":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"The database ID for {region} is {text}")
+                )
             case "delegate":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Delegate {text}")
+                )
             case "delegateauth":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Delegate Auth {text}")
+                )
             case "delegatevotes":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Delegate Votes {text}")
+                )
             case "dispatches":
                 pass
             case "embassies":
@@ -73,45 +89,87 @@ class FormatRegionInfo(Formatter):
             case "factbook":
                 pass
             case "flag":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"The flag of {region}", url=text)
+                )
             case "founded":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"{region} founded {text}")
+                )
             case "foundedtime":
-                pass
+                temp = float(text)
+                dt = datetime.fromtimestamp(temp)
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"The region of {region} was founded at {dt}.")
+                )
             case "founder":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Founder of {region}: {text}")
+                )
             case "founderauth":
                 pass
             case "gavote":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title=f"{region}'s status in the General Assembly is currently {text}."
+                    )
+                )
             case "happenings":
                 pass
             case "history":
                 pass
             case "lastupdate":
-                pass
+                temp = float(text)
+                dt = datetime.fromtimestamp(temp)
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"The region of {region} was last updated at {dt}.")
+                )
             case "messages":
                 pass
             case "name":
-                pass
+                await interaction.response.send_message(embed=discord.Embed(title=text))
             case "nations":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Nations in {region}: {text}")
+                )
             case "numnations":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"Number of nations in {region}: {text}")
+                )
             case "officers":
                 pass
             case "poll":
                 pass
             case "power":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(title=f"{region}'s power is {text}")
+                )
             case "scvote":
-                pass
+                if text:
+                    await interaction.response.send_message(
+                        embed=discord.Embed(
+                            title=f"{region}'s status on the Security Council is currently {text}."
+                        )
+                    )
+                else:
+                    await interaction.response.send_message(
+                        embed=discord.Embed(
+                            title=f"{region} is not a member of the Security Council."
+                        )
+                    )
             case "tags":
                 pass
             case "wabadges":
                 pass
             case "zombie":
-                pass
+                await interaction.response.send_message(
+                    embed=discord.Embed(
+                        title=f"Zombie information for {region}",
+                        description=f"Industry: {root[0].findtext('SURVIVORS')}\
+                        \nZombies: {root[0].findtext('ZOMBIES')}\
+                        \nDead: {root[0].findtext('DEAD')}\n",
+                    )
+                )
 
         await interaction.response.send_message(
             embed=discord.Embed(
