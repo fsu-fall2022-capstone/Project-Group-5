ec763248d13d07c18bb33c14cdef562a04a58116
CoderJoshDK <74162303+CoderJoshDK@users.noreply.github.com>
2022-12-07 17:11:02 -0500

    Closer to PEP8 standards

--- ns_bot/controllers/world_assembly_controller.py
+++ ns_bot/controllers/world_assembly_controller.py
@@ -1,9 +1,7 @@
-import traceback
-from typing import Literal, Optional
+from typing import Optional
 
 import discord
 from discord import app_commands
-from discord.ext import commands
 
 from ns_bot.controllers.base_nationstate_controller import BaseNationstateController
 from ns_bot.data.shards import (
--- ns_bot/formatting/region_info.py
+++ ns_bot/formatting/region_info.py
@@ -1,13 +1,9 @@
 import xml.etree.ElementTree as ET
 from datetime import datetime
-from io import BytesIO
 
 import discord
-from aiohttp import ClientSession
-from PIL import Image
 
 from ns_bot.formatting import Formatter
-from ns_bot.utils.wrappers import async_wrapper
 
 
 class FormatRegionInfo(Formatter):
